a9e16474a3b70f347abaa675ae580e8e852264ef
Misha31536 <e4ik@ya.ru>
2024-07-06 01:02:54 +0700

    24/07/06
    добавил методы add, radd, iadd

--- module_5_3.py
+++ module_5_3.py
@@ -1,11 +1,13 @@
-from random import randint # импортируем модуль для генерации случайных чисел
+from random import randint  # импортируем модуль для генерации случайных чисел
+
+
 class House:
 
-    def __init__(self, name, number_of_floors): # указываем атрибуты
+    def __init__(self, name, number_of_floors: int):  # указываем атрибуты
         self.name = name
         self.number_of_floors = number_of_floors
 
-    def go_to(self, new_floor): # создаем метод
+    def go_to(self, new_floor):  # создаем метод
         if new_floor < 1 or new_floor > self.number_of_floors:
             print(f'В {self.name} Этажа {new_floor} не существует')
         else:
@@ -19,49 +21,85 @@ class House:
     def __str__(self):
         return self.name
 
-    # def __eq__(self, other): # Равенство
-    #     if isinstance(other, House) and isinstance(other.number_of_floors, int):
-    #         return self.number_of_floors == other.number_of_floors
-    #     else:
-    #         return print(f'нельзя сравнить {self.number_of_floors} и {other.number_of_floors}')
-    #
-    def __lt__(self, other): # Меньше
-        if isinstance(other, House) and isinstance(other.number_of_floors, int):
+    def __add__(self, value):
+        if not isinstance(value.number_of_floors, (int, House)):
+            raise ArithmeticError("Правый операнд должен быть типом int или объектом House")
+
+        return self.number_of_floors + value
+
+    def __iadd__(self, value):
+        if not isinstance(value.number_of_floors, (int, House)):
+            raise ArithmeticError("Правый операнд должен быть типом int или объектом House")
+
+        nf = value if isinstance(value, int) else value.number_of_floors
+        self.number_of_floors += nf
+
+        return self
+
+    def __radd__(self, value):
+        if not isinstance(value.number_of_floors, (int, House)):
+            raise ArithmeticError("Правый операнд должен быть типом int или объектом House")
+
+        return self.number_of_floors + value
+
+    def __eq__(self, other):  # Равенство
+        if not isinstance(other.number_of_floors, (int, House)):
+            return print(f'нельзя сравнить {self.number_of_floors} и {other.number_of_floors}')
+        else:
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):  # Меньше
+        if not isinstance(other.number_of_floors, (int, House)):
+            return print(f'нельзя сравнить {self.number_of_floors} и {other.number_of_floors}')
+        else:
             return self.number_of_floors < other.number_of_floors
-        else:
+
+    def __le__(self, other):  # Меньше или равно
+        if not isinstance(other.number_of_floors, (int, House)):
             return print(f'нельзя сравнить {self.number_of_floors} и {other.number_of_floors}')
-    #
-    def __le__(self, other): # Меньше или равно
-        if isinstance(other, House) and isinstance(other.number_of_floors, int):
+        else:
             return self.number_of_floors <= other.number_of_floors
-        else:
-            return print(f'нельзя сравнить {self.number_of_floors} и {other.number_of_floors}')
-    #
+
     # def __gt__(self, other): # Больше
     #     if isinstance(other, House) and isinstance(other.number_of_floors, int):
     #         return self.number_of_floors > other.number_of_floors
     #     else:
     #         return print(f'нельзя сравнить {self.number_of_floors} и {other.number_of_floors}')
 
+
 elbrus = House('ЖК Эльбрус', 30)
+
+
 dom = House('Дом в деревне', 3)
+
 elbrus.go_to(randint(1, 100))
 dom.go_to(randint(1, 100))
-#__len__
+# __len__
 print(f' всего этажей в {elbrus} {len(elbrus)}')
 print(f' всего этажей в {dom} {len(dom)}')
-#__str__
+# __str__
 print(f' Название комплекса {elbrus}')
 print(f' Название комплекса {dom}')
 # равенство
-print(elbrus == dom)
+print(f' равенство {elbrus.number_of_floors} = {dom.number_of_floors} : {elbrus == dom}')
 # меньше
-print(elbrus < dom)
+print(f' меньше {elbrus.number_of_floors} < {dom.number_of_floors} : {elbrus < dom}')
 # меньше или равно
-print(elbrus <= dom)
+print(f' меньше или равно {elbrus.number_of_floors} <= {dom.number_of_floors} : {elbrus <= dom}')
 # Больше
-print(elbrus > dom)
+print(f' Больше {elbrus.number_of_floors} > {dom.number_of_floors} : {elbrus > dom}')
 # Больше или равно
-print(elbrus >= dom)
+print(f' больше или равно {elbrus.number_of_floors} => {dom.number_of_floors} : {elbrus >= dom}')
 # Не равно
-print(elbrus != dom)
+print(f' не равно {elbrus.number_of_floors} != {dom.number_of_floors} : {elbrus != dom}')
+#  add
+dom.number_of_floors = dom.number_of_floors + 27
+print(f' Название {dom} количество этажей {len(dom)}')
+print(f' равенство {elbrus.number_of_floors} = {dom.number_of_floors} : {elbrus == dom}')
+# iadd
+dom.number_of_floors += 10
+print(f' Название {dom} количество этажей {len(dom)}')
+# radd
+dom.number_of_floors = 10 + dom.number_of_floors
+print(f' Название {dom} количество этажей {len(dom)}')
+
